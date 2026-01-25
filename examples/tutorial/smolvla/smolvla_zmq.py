10e3013e7ff9909f223ad9dc8b2023efebcd8eed
shenjie <shenjie0203@outlook.com>
2026-01-25 19:57:21 +0100

    feat: adapt to hf dataset shenj/piper_pickplace

--- examples/tutorial/smolvla/smolvla_zmq.py
+++ examples/tutorial/smolvla/smolvla_zmq.py
@@ -14,9 +14,11 @@ from lerobot.policies.utils import build_inference_frame, make_robot_action
 
 
 MODEL_ID = "lerobot/smolvla_base"
+MODEL_PATH = "/home/shenjie/Documents/smolvla_result_20260124/checkpoints/last/pretrained_model"
 
 MAX_EPISODES = 5
 MAX_STEPS_PER_EPISODE = 20
+N_ACTION_STEPS = 20 # n_action_steps in the training params
 STORE_IMAGES = False # whether store preprocessed images from socket to /logs
 IMAGES_STORE_PATH = Path("logs")
 
@@ -32,15 +34,8 @@ JOINT_ORDER = [
 ACTION_FEATURES = {
     'action': {
         'dtype': 'float32',
-        'shape': (6,),
-        'names': [
-            'shoulder_pan.pos', 
-            'shoulder_lift.pos', 
-            'elbow_flex.pos', 
-            'wrist_flex.pos', 
-            'wrist_roll.pos', 
-            'gripper.pos',
-        ],
+        'shape': (8,),
+        'names': JOINT_ORDER,
     },
 }
 
@@ -49,17 +44,10 @@ ACTION_FEATURES = {
 OBS_FEATURES = {
     'observation.state': {
         'dtype': 'float32',
-        'shape': (6,),
-        'names': [
-            'shoulder_pan.pos', 
-            'shoulder_lift.pos', 
-            'elbow_flex.pos', 
-            'wrist_flex.pos', 
-            'wrist_roll.pos', 
-            'gripper.pos',
-        ],
+        'shape': (8,),
+        'names': JOINT_ORDER,
     },
-    'observation.images.camera1': {
+    'observation.images.top': {
         'dtype': 'video',
         'shape': (480, 640, 3),
         'names': [
@@ -68,7 +56,7 @@ OBS_FEATURES = {
             'channels',
         ],
     },
-    'observation.images.camera2': {
+    'observation.images.wrist': {
         'dtype': 'video',
         'shape': (480, 640, 3),
         'names': [
@@ -80,15 +68,18 @@ OBS_FEATURES = {
 }
 
 DATASET_FEATURES = {**ACTION_FEATURES, **OBS_FEATURES}
+print("Dataset features:", DATASET_FEATURES)
 
-# radian range of so100 joints in Isaac Sim
+# radian range of piper joints in Isaac Sim
 SIMULATION_RANGE = {
-    'shoulder_pan.pos': {'sim_min': -2.0, 'sim_max': 2.0},
-    'shoulder_lift.pos': {'sim_min': 0.0, 'sim_max': 3.5},
-    'elbow_flex.pos': {'sim_min': -3.142, 'sim_max': 0.0},
-    'wrist_flex.pos': {'sim_min': -2.5, 'sim_max': 1.2},
-    'wrist_roll.pos': {'sim_min': -3.142, 'sim_max': 3.142},
-    'gripper.pos': {'sim_min': -0.2, 'sim_max': 2.0},
+    'joint1': {'sim_min': -2.618, 'sim_max': 2.618},
+    'joint2': {'sim_min': 0.0, 'sim_max': 3.14},
+    'joint3': {'sim_min': -2.697, 'sim_max': 0.0},
+    'joint4': {'sim_min': -1.832, 'sim_max': 1.832},
+    'joint5': {'sim_min': -1.22, 'sim_max': 1.22},
+    'joint6': {'sim_min': -3.14, 'sim_max': 3.14},
+    'joint7': {'sim_min': 0, 'sim_max': 1.5},
+    'joint8': {'sim_min': 0.0, 'sim_max': 0.04},
 }
 
 def rad2pos(rad: float, joint_name: str):
@@ -101,14 +92,28 @@ def rad2pos(rad: float, joint_name: str):
     """
     sim_min = SIMULATION_RANGE[joint_name]['sim_min']
     sim_max = SIMULATION_RANGE[joint_name]['sim_max']
-    pos = ((rad - sim_min) / (sim_max - sim_min)) * 200 - 100
+    # pos = ((rad - sim_min) / (sim_max - sim_min)) * 200 - 100
+
+    # For piper
+    if rad < sim_min:
+        rad = sim_min
+    if rad > sim_max:
+        rad = sim_max
+    pos = np.rad2deg(rad)  # convert to degrees
     return pos
 
 def pos2rad(pos: float, joint_name: str):
     """Convert the motor pos into real radian."""
     sim_min = SIMULATION_RANGE[joint_name]['sim_min']
     sim_max = SIMULATION_RANGE[joint_name]['sim_max']
-    rad = sim_min + (pos + 100) / 200 * (sim_max - sim_min)
+    # rad = sim_min + (pos + 100) / 200 * (sim_max - sim_min)
+
+    # For piper
+    rad = np.deg2rad(pos)  # convert degrees to radian
+    if rad < sim_min:
+        rad = sim_min
+    if rad > sim_max:
+        rad = sim_max
     return rad
 
 
@@ -126,7 +131,7 @@ if __name__ == "__main__":
 
     device = torch.device("cuda")
 
-    model = SmolVLAPolicy.from_pretrained(MODEL_ID)
+    model = SmolVLAPolicy.from_pretrained(MODEL_PATH) # MODEL_ID if want to retrieve from hub
     # model = model.to(device)
     # model.eval()
     print("Model device:", next(model.parameters()).device)
@@ -143,12 +148,12 @@ if __name__ == "__main__":
 
     preprocess, postprocess = make_pre_post_processors(
         policy_cfg=model.config,
-        pretrained_path=MODEL_ID,
+        pretrained_path=MODEL_PATH,   # MODEL_ID if want to retrieve from hub
         preprocessor_overrides={"device_processor": {"device": str(device)}},
     )
 
-    task = "pick the red block"
-    robot_type = "so100_follower"
+    task = "Pick up the red cube and place it in the box"
+    robot_type = "piper_isaac_sim420"
 
     print(f"Current task: {task}")
 
@@ -158,6 +163,7 @@ if __name__ == "__main__":
         payload = obs_socket.recv()   # one npz blob
         buf = io.BytesIO(payload)
         data = np.load(buf)
+        print("Received data keys:", list(data.keys()))
 
         ts = float(data["ts"][0])
         joints = data["joints"].astype(np.float32)
@@ -199,14 +205,16 @@ if __name__ == "__main__":
         # }
 
         obs = {
-            "shoulder_pan.pos": rad2pos(rad=joints[0], joint_name="shoulder_pan.pos"),
-            "shoulder_lift.pos": rad2pos(rad=joints[1], joint_name="shoulder_lift.pos"),
-            "elbow_flex.pos": rad2pos(rad=joints[2], joint_name="elbow_flex.pos"),
-            "wrist_flex.pos": rad2pos(rad=joints[3], joint_name="wrist_flex.pos"),
-            "wrist_roll.pos": rad2pos(rad=joints[4], joint_name="wrist_roll.pos"),
-            "gripper.pos": rad2pos(rad=joints[5], joint_name="gripper.pos"),
-            "camera1": img1,
-            "camera2": img2,
+            "joint1": rad2pos(rad=joints[0], joint_name="joint1"),      # When collecting data in Isaac Sim, we should just keep the radian!!
+            "joint2": rad2pos(rad=joints[1], joint_name="joint2"),
+            "joint3": rad2pos(rad=joints[2], joint_name="joint3"),
+            "joint4": rad2pos(rad=joints[3], joint_name="joint4"),
+            "joint5": rad2pos(rad=joints[4], joint_name="joint5"),
+            "joint6": rad2pos(rad=joints[5], joint_name="joint6"),
+            "joint7": joints[5] * 100, # scale up the gripper open/close command
+            "joint8": joints[6] * 100,
+            "top": img1,
+            "wrist": img2,
         }
 
         # TODO: check what viewpoint angle and distance to robot (i.e. pose) should the camera have
@@ -229,7 +237,7 @@ if __name__ == "__main__":
         obs = preprocess(obs_frame)
 
         # save images in obs
-        if STORE_IMAGES and count % 10 == 0:
+        if STORE_IMAGES and count % N_ACTION_STEPS == 0:
             # cam1
             cam1_img = obs['observation.images.camera1'].cpu().detach().numpy().squeeze().reshape(480, 640, 3)
             cam1_img_uint8 = (cam1_img * 255).astype(np.uint8)
@@ -256,15 +264,17 @@ if __name__ == "__main__":
         action = make_robot_action(action, DATASET_FEATURES)
 
         action_array = np.array([
-            pos2rad(pos=action["shoulder_pan.pos"], joint_name="shoulder_pan.pos"),
-            pos2rad(pos=action["shoulder_lift.pos"], joint_name="shoulder_lift.pos"),
-            pos2rad(pos=action["elbow_flex.pos"], joint_name="elbow_flex.pos"),
-            pos2rad(pos=action["wrist_flex.pos"], joint_name="wrist_flex.pos"),
-            pos2rad(pos=action["wrist_roll.pos"], joint_name="wrist_roll.pos"),
-            pos2rad(pos=action["gripper.pos"], joint_name="gripper.pos"),
+            pos2rad(pos=action["joint1"], joint_name="joint1"),
+            pos2rad(pos=action["joint2"], joint_name="joint2"),
+            pos2rad(pos=action["joint3"], joint_name="joint3"),
+            pos2rad(pos=action["joint4"], joint_name="joint4"),
+            pos2rad(pos=action["joint5"], joint_name="joint5"),
+            pos2rad(pos=action["joint6"], joint_name="joint6"),
+            action["joint7"] / 100.0,   # scale down the gripper
+            action["joint8"] / 100.0,
         ],dtype=np.float32)
 
-        if count % 10 == 0:
+        if count % N_ACTION_STEPS == 0:
             print_action = ', '.join(f"{value:2f}" for value in action_array.tolist())
             print(f"Returned actions: [{print_action}]")
 
